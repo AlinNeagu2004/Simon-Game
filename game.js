70746ef42188811abcea2ca003f30842d04520f8
Alin Neagu <alinneagu10@gmail.com>
2021-08-23 12:36:20 +0300

    Create A New Pattern.

--- game.js
+++ game.js
@@ -1,1 +1,9 @@
-alert("test");
+var buttonColours = ["red", "blue", "green", "yellow"];
+
+var gamePattern = [];
+
+function nextSequence() {
+  var randomNumber = Math.floor(Math.random() * 4);
+  var randomChosenColour = buttonColours[randomNumber];
+  gamePattern.push(randomChosenColour);
+}
