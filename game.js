36ba48c4526634c70baed1ad263eb2677725bc0a
Alin Neagu <alinneagu10@gmail.com>
2021-08-23 14:19:54 +0300

    Show the Sequence to the User with Animations and Sounds.

--- game.js
+++ game.js
@@ -1,3 +1,5 @@
+// Create A New Pattern
+
 var buttonColours = ["red", "blue", "green", "yellow"];
 
 var gamePattern = [];
@@ -6,4 +8,14 @@ function nextSequence() {
   var randomNumber = Math.floor(Math.random() * 4);
   var randomChosenColour = buttonColours[randomNumber];
   gamePattern.push(randomChosenColour);
+
+  // Show the Sequence to the User with Animations and Sounds
+
+  $("#" + randomChosenColour)
+    .fadeIn(100)
+    .fadeOut(100)
+    .fadeIn(100);
+
+  var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
+  audio.play();
 }
