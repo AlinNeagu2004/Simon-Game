4d974dc3de66671c9ee66176c16c26b09a6090dd
Alin Neagu <alinneagu10@gmail.com>
2021-08-25 14:26:38 +0300

    Game Over + minor changes.

--- game.js
+++ game.js
@@ -1,18 +1,20 @@
 // Create A New Pattern
 
 var buttonColours = ["red", "blue", "green", "yellow"];
+
 var gamePattern = [];
 var userClickedPattern = [];
 
+var started = false;
 var level = 0;
 
 // Start the Game
 
-$(document).one("keydown", function (event) {
-  if (event.shiftKey) {
+$(document).keypress(function () {
+  if (!started) {
+    $("#level-title").text("Level " + level);
     nextSequence();
-  } else {
-    nextSequence();
+    started = true;
   }
 });
 
@@ -23,10 +25,33 @@ $(".btn").click(function () {
   userClickedPattern.push(userChosenColour);
   playSound(userChosenColour);
   animatePress(userChosenColour);
-  setTimeout(nextSequence, 1000);
+  checkAnswer(userClickedPattern.length - 1);
 });
 
+// Check the User's Answer
+
+function checkAnswer(currentLevel) {
+  if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+    console.log("success");
+    if (userClickedPattern.length === gamePattern.length) {
+      setTimeout(function () {
+        nextSequence();
+      }, 1000);
+    }
+  } else {
+    // Game Over
+    playSound("wrong");
+    $("body").addClass("game-over");
+    setTimeout(function () {
+      $("body").removeClass("game-over");
+    }, 200);
+    $("#level-title").text("Game Over, Press Any Key to Restart");
+  }
+}
+
 function nextSequence() {
+  userClickedPattern = [];
+
   var randomNumber = Math.floor(Math.random() * 4);
   var randomChosenColour = buttonColours[randomNumber];
   gamePattern.push(randomChosenColour);
